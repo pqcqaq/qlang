8c7759fe0da7f898b63c539632f721839dcb6ed4
qcqcqc <1220204124@zust.edu.cn>
2026-04-02 05:13:28 +0800

    async: add guarded tail inline forward await surface

--- crates/ql-cli/tests/executable_examples.rs
+++ crates/ql-cli/tests/executable_examples.rs
@@ -313,6 +313,11 @@ fn async_program_surface_examples_build_and_run() {
             source_relative: "ramdon_tests/async_program_surface_examples/53_async_main_guarded_const_backed_triple_root_triple_source_tail_direct_inline_forward_spawn.ql",
             expected_exit: 102,
         },
+        ExecutableExampleCase {
+            name: "async_main_guarded_const_backed_triple_root_triple_source_tail_direct_inline_forward_await",
+            source_relative: "ramdon_tests/async_program_surface_examples/54_async_main_guarded_const_backed_triple_root_triple_source_tail_direct_inline_forward_await.ql",
+            expected_exit: 104,
+        },
     ];
 
     assert_example_cases_run(
